81df830e4ea882c5e63fd2b0afba3adc4781b0b4
Aaron Lewis <amlewis@stanford.edu>
2011-04-30 20:02:56 -0700

    mexmaxflow.cpp successful compile

--- project2/mexmaxflow.cpp
+++ project2/mexmaxflow.cpp
@@ -12,8 +12,6 @@
 
 using namespace std;
 
-extern void _main();
-
 void mexFunction(
 		 int          nlhs,
 		 mxArray      *plhs[],
@@ -34,27 +32,28 @@ void mexFunction(
 
   double *backWeights = (double *) mxGetPr(prhs[0]);
   double *foreWeights = (double *) mxGetPr(prhs[1]);
-  double *smoothIndices = (double *) mxGetScalar(prhs[2]);
+  double *smoothIndices = (double *) mxGetPr(prhs[2]);
   double *smoothWeights = (double *) mxGetPr(prhs[3]);
   size_t numNodes = mxGetNumberOfElements(prhs[0]);
   size_t numDirections = mxGetN(prhs[2]);
 
   //Error checking
-  if (numNodes != mxGetNumberOfElements(prhs[1]) mexErrMsgIdAndTxt("MATLAB:mexmaxflow:argin", "Weight arrays must be same length");
+  if (numNodes != mxGetNumberOfElements(prhs[1])) mexErrMsgIdAndTxt("MATLAB:mexmaxflow:argin", "Weight arrays must be same length");
   if (numNodes != mxGetM(prhs[2])) mexErrMsgIdAndTxt("MATLAB:mexmaxflow:argin", "Number of rows for edge matrix does not match number of nodes");
   if (mxGetN(prhs[2]) != mxGetN(prhs[3]) || mxGetM(prhs[2]) != mxGetM(prhs[3])) mexErrMsgIdAndTxt("MATLAB:mexmaxflow:argin", "Edge weights matrix does not match edge indices matrix");
 
   //Create and fill graph
   typedef Graph<double, double, double> GraphType;
-  GraphType *g = new GraphType(numNodes,numNodes*numDirections);
-  g->add_nodes(numNodes);
+  GraphType *g = new GraphType(((int)numNodes),((int)(numNodes*numDirections)));
+  g->add_node(((int)numNodes));
 
   //Background is source, foreground is sink
-  for (size_t i=0: i<numNodes; i++)
+  for (size_t i=0; i<numNodes; i++)
   {
 	g->add_tweights(i, backWeights[i], foreWeights[i]);
 	for (size_t j=0; j<numDirections; j++)
 	{
+        mexPrintf("Value1 = %g\n", smoothIndices[numNodes*j + i]);
 		int edgeIndex = (int)(smoothIndices[numNodes*j + i]-1);
 		double edgeWeight = smoothWeights[numNodes*j + i];
 		if (edgeIndex < 0) break;
