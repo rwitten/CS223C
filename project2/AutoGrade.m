c17977ae80e9f22f61d0f3095e1c70ee70f5ba8e
Aaron Lewis <amlewis@stanford.edu>
2011-05-06 22:14:49 -0700

    Merging

--- project2/AutoGrade.m
+++ project2/AutoGrade.m
@@ -3,19 +3,19 @@ load('imgNames.mat', 'imageNames');
 
 %Grab cut paramters
 params.K = 5;
-params.foreK = 10;
-params.backK = 10;
+params.foreK = 5;
+params.backK = 5;
 params.numDirections = 8;
 params.gamma = 50;
 params.betaColCoeff = 2;
 params.superEdgeSharpness = 10;
 params.TotalIters = 20;
-params.MaxIter = 1;
-params.initIter = 1;
+params.MaxIter = 2;
+params.initIter = 2;
 params.sharpAlpha = 0.2;
 params.beInteractive = false;
 params.useGMTools = true;
-params.superSharpEdges = false;
+params.superSharpEdges = true;
 params.useGT = true;
 
 totalScore = 0;
